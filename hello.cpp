bb5ed485243e6fd5524af516c9348bf54f7167c9
AbdurCodes <aravman@gmail.com>
2024-02-15 20:03:18 +0500

    my first c++ program

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    cout << "Hello Universe! I am learning programming.";
+    
+}
+
+
+
+
+
+// or
+// #include <iostream>
+
+// main() {
+//     std::cout << "Hello Universe! I am learning programming.";
+    
+// }

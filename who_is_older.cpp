15fd0bac51a9c2ed84487bebb3b577f4578efa53
AbdurCodes <aravman@gmail.com>
2024-02-17 00:54:15 +0500

    who is older with user input

--- who_is_older.cpp
+++ who_is_older.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 main(){
     int AmirAge, AmmaraAge;
-    AmirAge = 33;
-    AmmaraAge = 25;
+
+    // though implicitly, the following values are 0 but it is better to be explicit
+    AmirAge = 0;
+    AmmaraAge = 0;
+
+    cout << "Amir enter your age plz: ";
+    cin >> AmirAge;
+    cout << "Ammara enter your age plz: ";
+    cin >> AmmaraAge;
 
     if (AmirAge > AmmaraAge) {
         cout << "Amir is older than Ammara";

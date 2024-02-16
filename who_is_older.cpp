d7e3f969664539074012c44899c0d42ed259a7a9
AbdurCodes <aravman@gmail.com>
2024-02-17 00:46:44 +0500

    simple who is older program in cpp

---
+++ who_is_older.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int AmirAge, AmmaraAge;
+    AmirAge = 33;
+    AmmaraAge = 25;
+
+    if (AmirAge > AmmaraAge) {
+        cout << "Amir is older than Ammara";
+    }
+    else {
+        cout << "Ammara is older than Amir";
+    }
+}

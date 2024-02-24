991fc27d1b773358ac9d8cc2b122863378b13a1f
AbdurCodes <aravman@gmail.com>
2024-02-24 23:30:40 +0500

    arrays in functions are called by reference

---
+++ arrays_func_call_by_reference.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// arrays in functions are called by reference
+
+void myFunc (int[], int);
+void myFunc (int myarray [], int arraySize){
+        for (int i=0; i<arraySize; i++){
+            myarray[i] = i+2;
+        }
+    }
+
+main(){
+    const int arraySize = 7;
+
+    int numsArray [arraySize];
+
+    myFunc(numsArray, arraySize);
+
+    // we can see the values of the arrays's elements are changed
+    for (int num : numsArray){
+        cout << num << " ";
+    }
+
+
+    
+
+    
+}
